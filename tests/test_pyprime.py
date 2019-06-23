5e32866e2b0f2b90a09c4e2efba1a2a23f166cd2
t nkgw <me@example.com>
2019-06-23 22:42:55 +0900

    modify test_pyprime.py

--- tests/test_pyprime.py
+++ tests/test_pyprime.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from pyprime import pyprime
 
