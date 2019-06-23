cf10aee0347c8ed3597a122745657230eca14b80
t nkgw <me@example.com>
2019-06-23 20:22:54 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,22 @@
 # -*- coding: utf-8 -*-
 
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
 
-
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    name='pyprime',
+    version='0.0.1',
+    description='Python packege for probabilistic primarity test',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='tkm',
+    author_email='unknown',
+    install_requires=['numpy'],
+    url='https://github.com/tkm-central/pyprime',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
